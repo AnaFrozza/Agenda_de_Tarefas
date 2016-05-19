f28429017abf9efd86af7d74bec2ccbedd3afcfe
Ana <annafrozza@gmail.com>
2016-05-19 00:11:53 -0300

    Teste

---
+++ src/main/AgendaDeTarefas.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import agendadetarefas.Usuario;
+import agendadetarefas.Tarefa;
+import agendadetarefas.DividirTarefa;
+/**
+ *
+ * @author Ana
+ */
+public class AgendaDeTarefas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Usuario morador = new Usuario();
+        Tarefa tarefa = new Tarefa();
+        DividirTarefa ran = new DividirTarefa();
+    }
+    
+}
